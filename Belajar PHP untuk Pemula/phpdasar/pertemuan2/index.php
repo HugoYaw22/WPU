609adcdc4db4669cbe84d65ec9a61b744128d15a
HugoYaw22 <hugoyaw22@gmail.com>
2021-07-07 17:02:24 +0700

    dasdasasasd

---
+++ Belajar PHP untuk Pemula/phpdasar/pertemuan2/index.php
@@ -0,0 +1,27 @@
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// print "Doctor Accounting";
+//echo 1 + 1;
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// $nama_depan = "Hugo";
+// $nama_belakang = "YAW";
+// echo $nama_depan . " " . $nama_belakang;
+
+//var_dump(1 > 5);
+// var_dump (1 == "1");
+
+$x = 10;
+var_dump($x < )
+
+?>
+
