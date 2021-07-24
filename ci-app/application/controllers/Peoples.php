f464fec5e487d16b9f498964e32baab3d3c73b6e
HugoYaw22 <hugoyaw22@gmail.com>
2021-07-24 14:12:13 +0700

    mau belajar laravel

--- ci-app/application/controllers/Peoples.php
+++ ci-app/application/controllers/Peoples.php
@@ -3,70 +3,39 @@
        
         public function index()
         {
-            $data['judul'] = 'Daftar Mahasiswa';
-            $data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
-            if ( $this->input->post('keyword') ) {
-                $data['mahasiswa'] = $this->Mahasiswa_model->cariDataMahasiswa();
+            $data['judul'] = 'List of Peoples';
+
+            $this->load->model('Peoples_model', 'peoples');
+
+            // load library
+            $this->load->library('pagination');
+
+            // ambil data keyword
+            if($this->input->post('submit')) {
+                $data['keyword'] = $this->input->post('keyword');
+                $this->session->set_userdata('keyword', $data['keyword']);
+            } else {
+                $data['keyword'] = $this->session->userdata('keyword');
             }
+
+            // config
+            $this->db->like('name', $data['keyword']);
+            $this->db->or_like('address', $data['keyword']);
+            $this->db->or_like('email', $data['keyword']);
+            $this->db->from('peoples');
+            $config['total_rows'] = $this->db->count_all_results();
+            $data['total_rows'] = $config['total_rows'];
+            $config['per_page'] = 8;
+
+            // initialize
+            $this->pagination->initialize($config);
+            $data['start'] = $this->uri->segment(3);
+            $data['peoples'] = $this->peoples->getPeoples($config['per_page'], $data['start'], $data['keyword']);
+
             $this->load->view('templates/header', $data);
-            $this->load->view('mahasiswa/index', $data);
+            $this->load->view('peoples/index', $data);
             $this->load->view('templates/footer');
         }
 
-        public function tambah()
-        {
-            $data['judul'] = 'Form Tambah Data Mahasiswa';
-
-            $this->form_validation->set_rules('nama', 'Nama', 'required');
-            $this->form_validation->set_rules('nrp', 'NRP', 'required|numeric');
-            $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-
-            if ( $this->form_validation->run() == FALSE) {
-                $this->load->view('templates/header', $data);
-                $this->load->view('mahasiswa/tambah');
-                $this->load->view('templates/footer');
-            } else {
-                $this->Mahasiswa_model->tambahDataMahasiswa();
-                $this->session->set_flashdata('flash', 'Ditambahkan');
-                redirect('mahasiswa');
-            }
-        }
-
-        public function hapus($id)
-        {
-            $this->Mahasiswa_model->hapusDataMahasiswa($id);
-            $this->session->set_flashdata('flash', 'Dihapus');
-            redirect('mahasiswa');
-        }
-
-        public function detail($id)
-        {
-            $data['judul'] = 'Detail Data Mahasiswa';
-            $data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
-            $this->load->view('templates/header', $data);
-            $this->load->view('mahasiswa/detail', $data);
-            $this->load->view('templates/footer');
-        }
-
-        public function ubah($id)
-        {
-            $data['judul'] = 'Form Ubah Data Mahasiswa';
-            $data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
-            $data['jurusan'] = ['Teknik Informatika', 'Teknik Mesin', 'Teknik Planologi', 'Teknik Pangan', 'Teknik Lingkungan'];
-
-            $this->form_validation->set_rules('nama', 'Nama', 'required');
-            $this->form_validation->set_rules('nrp', 'NRP', 'required|numeric');
-            $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-
-            if ( $this->form_validation->run() == FALSE) {
-                $this->load->view('templates/header', $data);
-                $this->load->view('mahasiswa/ubah', $data);
-                $this->load->view('templates/footer');
-            } else {
-                $this->Mahasiswa_model->ubahDataMahasiswa();
-                $this->session->set_flashdata('flash', 'Diubah');
-                redirect('mahasiswa');
-            }
-        }
     }
 ?>
