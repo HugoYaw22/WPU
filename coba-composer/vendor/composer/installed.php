f5a1bf2ed022da7d6a73a4d131a08e7ee1ed1704
HugoYaw22 <hugoyaw22@gmail.com>
2021-07-26 19:03:17 +0700

    mau pindah kamarrrrr

---
+++ coba-composer/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f464fec5e487d16b9f498964e32baab3d3c73b6e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f464fec5e487d16b9f498964e32baab3d3c73b6e',
+    ),
+    'fzaninotto/faker' => 
+    array (
+      'pretty_version' => 'v1.5.0',
+      'version' => '1.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd0190b156bcca848d401fb80f31f504f37141c8d',
+    ),
+  ),
+);
