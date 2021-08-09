0c452a5e89997386edf84ffc91766859d5932a1c
HugoYaw22 <hugoyaw22@gmail.com>
2021-08-09 14:39:38 +0700

    aaa

---
+++ applications/kedaicatkarman/resources/views/point.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('container')
+    <h1>Halaman Point</h1>
+
+    
+@endsection
--- applications/kedaicatkarman/routes/web.php
+++ applications/kedaicatkarman/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home']);
+});
+
+Route::get('/point', function() {
+    return view('point', ['title' => 'Point']);
 });
